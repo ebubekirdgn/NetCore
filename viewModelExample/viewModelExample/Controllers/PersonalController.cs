120514bde6d691b80ddee7e2dabcaaab816b2881
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-11-10 06:15:20 +0300

    update

---
+++ viewModelExample/viewModelExample/Controllers/PersonalController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using viewModelExample.Models.ViewModels;
+
+namespace viewModelExample.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonalCreateVM personal)
+        {
+            //.. ilgili islemler burada yapılır.
+            return View();
+        }
+    }
+}
---
+++ viewModelExample/viewModelExample/Models/ViewModels/PersonalCreateVM.cs
@@ -0,0 +1,9 @@
+﻿namespace viewModelExample.Models.ViewModels
+{
+    public class PersonalCreateVM
+    {
+        //ViewModel'da sadece taşınacak verinin propertyleri temsil edilir.
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
