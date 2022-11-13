a6bdacf983a0d68d8670bfe315d4a5b4989288c8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-11-13 10:28:52 +0300

    update

---
+++ configurationExample/configurationExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using configurationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace configurationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        MailInfo _mailInfo;
+        public HomeController(IOptions<MailInfo> mailInfo)
+        {
+            _mailInfo = mailInfo.Value;
+        }
+
+        public IActionResult Index()
+        { 
+            return View();
+        }
+     
+    }
+}
