5fdbf245ff5df5e94089f79062c74c5c96bcd206
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-11-13 10:54:07 +0300

    secretManagerExample

---
+++ secretManagerExample/secretManagerExample/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace secretManagerExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Index()
+        {
+            var user = _configuration["MailBilgileri:User"];
+            var password = _configuration["MailBilgileri:Password"];
+        }
+    }
+
+
+}
